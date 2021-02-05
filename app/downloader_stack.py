b8063ee8140e66a84dbd8b50861eb43fbe5f9c04
Ciaran Evans <ciaran@developmentseed.org>
2021-02-05 17:22:19 +0000

    Add cdk to debug aurora serverless

--- app/downloader_stack.py
+++ app/downloader_stack.py
@@ -1,6 +1,14 @@
 import os
 
-from aws_cdk import aws_ec2, aws_lambda, aws_lambda_python, aws_rds, aws_secretsmanager, aws_sqs, core
+from aws_cdk import (
+    aws_ec2,
+    aws_lambda,
+    aws_lambda_python,
+    aws_rds,
+    aws_secretsmanager,
+    aws_sqs,
+    core,
+)
 
 REPO_ROOT = os.path.dirname(os.path.abspath(__file__)).replace("app", "")
 
@@ -16,63 +24,125 @@ class DownloaderStack(core.Stack):
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        prod = True if stage == "PROD" else False
+
         vpc = aws_ec2.Vpc(
             self,
             id=f"{construct_id}-vpc",
-            cidr="10.0.0.0/24",
+            nat_gateways=0,
+            cidr="10.0.0.0/26",
+            subnet_configuration=[
+                aws_ec2.SubnetConfiguration(
+                    name="PublicSubnet1", subnet_type=aws_ec2.SubnetType.PUBLIC
+                )
+            ],
+            max_azs=3,
         )
 
-        # rds_subnet_group = aws_rds.SubnetGroup(
-        #     self,
-        #     id=f"{construct_id}-rds-subnet-group",
-        #     vpc=vpc,
-        #     vpc_subnets=aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PRIVATE)
-        # )
+        rds_subnet_group = aws_rds.SubnetGroup(
+            self,
+            id=f"{construct_id}-rds-subnet-group",
+            vpc=vpc,
+            vpc_subnets=aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType.PUBLIC),
+            description=f"Subnet group for {construct_id}-downloader-rds",
+        )
 
+        rds_security_group = aws_ec2.SecurityGroup(
+            self,
+            id=f"{construct_id}-rds-security-group",
+            vpc=vpc,
+            allow_all_outbound=True,
+            description=f"Security group for {construct_id}-downloader-rds",
+        )
+
+        rds_security_group.add_ingress_rule(
+            peer=aws_ec2.Peer.any_ipv4(),
+            connection=aws_ec2.Port.tcp(5432),
+            description="Allow all traffic for Postgres",
+        )
+
+        # Doesn't work, should be publicly available as in public subnets etc
+        downloader_rds = aws_rds.ServerlessCluster(
+            self,
+            id=f"{construct_id}-downloader-rds",
+            engine=aws_rds.DatabaseClusterEngine.aurora_postgres(
+            version=aws_rds.AuroraPostgresEngineVersion.VER_10_12
+        ),
+            vpc=vpc,
+            subnet_group=rds_subnet_group,
+            security_groups=[rds_security_group],
+            default_database_name="hlss2downloader",
+            enable_data_api=True,
+            removal_policy=core.RemovalPolicy.RETAIN
+            if prod
+            else core.RemovalPolicy.DESTROY,
+        )
+
+        # # Works - Publicly available but not serverless
         # downloader_rds = aws_rds.DatabaseCluster(
         #     self,
         #     id=f"{construct_id}-downloader-rds",
         #     engine=aws_rds.DatabaseClusterEngine.aurora_postgres(
-        #         version=aws_rds.AuroraPostgresEngineVersion.VER_11_9
+        #         version=aws_rds.AuroraPostgresEngineVersion.VER_10_12
         #     ),
         #     instance_props=aws_rds.InstanceProps(
         #         vpc=vpc,
+        #         security_groups=[rds_security_group],
+        #         publicly_accessible=True,
         #     ),
-        #     subnet_group=rds_subnet_group
+        #     subnet_group=rds_subnet_group,
+        #     default_database_name="hlss2downloader",
+        #     removal_policy=core.RemovalPolicy.RETAIN
+        #     if prod
+        #     else core.RemovalPolicy.DESTROY,
         # )
 
-        lambda_security_group = aws_ec2.SecurityGroup(
-            self,
-            id=f"{construct_id}-lambda-security-group",
-            vpc=vpc,
-            allow_all_outbound=True
+        core.CfnOutput(
+            self, id="downloader-ip", value=downloader_rds.cluster_endpoint.hostname
         )
+
+        # psycopg2_layer = aws_lambda.LayerVersion.from_layer_version_arn(
+        #     self,
+        #     id=f"{construct_id}-pyscopg2-layer",
+        #     layer_version_arn=(
+        #         "arn:aws:lambda:us-west-2:898466741470:layer:psycopg2-py38:1"
+        #     ),
+        # )
+
+        # migration_function = aws_lambda_python.PythonFunction(
+        #     self,
+        #     id=f"{construct_id}-migration-function",
+        #     entry="alembic",
+        #     handler="handler",
+        #     index="alembic_handler.py",
+        #     runtime=aws_lambda.Runtime.PYTHON_3_8,
+        #     memory_size=128,
+        #     timeout=core.Duration.minutes(5),
+        #     layers=[
+        #         psycopg2_layer,
+        #     ],
+        #     environment={"DB_CONNECTION_SECRET_ARN": downloader_rds.secret.secret_arn},
+        # )
+
+        # downloader_rds.secret.grant_read(migration_function)
+
+        # migration_function_resource = core.CustomResource(
+        #     self,
+        #     id=f"{construct_id}-migration-function-resource",
+        #     service_token=migration_function.function_arn,
+        # )
+
+        # lambda_security_group = aws_ec2.SecurityGroup(
+        #     self,
+        #     id=f"{construct_id}-lambda-security-group",
+        #     vpc=vpc,
+        #     allow_all_outbound=True,
+        # )
 
         # downloader_rds.connections.allow_default_port_from(
         #     other=lambda_security_group,
         #     description="Allow access from Lambda"
         # )
-
-        aws_lambda.Function(
-            self,
-            "test",
-            code=aws_lambda.Code.from_inline(
-                """
-import urllib.request as ur
-import json
-
-def handler(event, context):
-    resp = ur.urlopen('https://jsonplaceholder.typicode.com/todos/1')
-    print(json.load(resp))
-                """
-            ),
-            handler="index.handler",
-            runtime=aws_lambda.Runtime.PYTHON_3_7,
-            memory_size=128,
-            timeout=core.Duration.minutes(5),
-            vpc=vpc,
-            security_group=lambda_security_group
-        )
 
         # to_download_queue = aws_sqs.Queue(
         #     self,
@@ -92,23 +162,42 @@ def handler(event, context):
         # db_layer = aws_lambda_python.PythonLayerVersion(
         #     self,
         #     id=f"{construct_id}-db-layer",
-        #     entry=os.path.join(REPO_ROOT, "layers", "db"),
+        #     entry="layers/db",
         #     compatible_runtimes=[aws_lambda.Runtime.PYTHON_3_8],
         # )
 
-        # _ = aws_lambda_python.PythonFunction(
+        # test_function = aws_lambda.Function(
         #     self,
-        #     id=f"{construct_id}-link-fetcher",
-        #     entry=os.path.join(REPO_ROOT, "lambdas", "link_fetcher"),
-        #     index="handler.py",
-        #     handler="handler",
-        #     layers=[db_layer, psycopg2_layer],
-        #     memory_size=1024,
-        #     timeout=core.Duration.minutes(5),
+        #     "test",
+        #     code=aws_lambda.Code.from_asset(
+        #         path="guff"
+        #     ),
+        #     handler="testhandler.handler",
         #     runtime=aws_lambda.Runtime.PYTHON_3_8,
+        #     memory_size=128,
+        #     timeout=core.Duration.minutes(5),
+        #     layers=[db_layer, psycopg2_layer],
         #     environment={
-        #         "STAGE": stage,
-        #         "TO_DOWNLOAD_SQS_QUEUE_URL": to_download_queue.queue_url,
+        #         "DB_CONNECTION_SECRET_ARN": downloader_rds.secret.secret_arn,
         #     },
-        #     vpc=vpc
         # )
+
+        # downloader_rds.secret.grant_read(test_function)
+
+
+# _ = aws_lambda_python.PythonFunction(
+#     self,
+#     id=f"{construct_id}-link-fetcher",
+#     entry=os.path.join(REPO_ROOT, "lambdas", "link_fetcher"),
+#     index="handler.py",
+#     handler="handler",
+#     layers=[db_layer, psycopg2_layer],
+#     memory_size=1024,
+#     timeout=core.Duration.minutes(5),
+#     runtime=aws_lambda.Runtime.PYTHON_3_8,
+#     environment={
+#         "STAGE": stage,
+#         "TO_DOWNLOAD_SQS_QUEUE_URL": to_download_queue.queue_url,
+#     },
+#     vpc=vpc
+# )
