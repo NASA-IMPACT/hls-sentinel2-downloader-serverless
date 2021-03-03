ae4ec05e5eec53514a6d7e0208986433bd238ae0
Ciaran Evans <ciaran@developmentseed.org>
2021-03-03 17:37:43 +0000

    Add region to test secretsmanager

--- lambdas/downloader/tests/conftest.py
+++ lambdas/downloader/tests/conftest.py
@@ -53,7 +53,7 @@ def aws_credentials(monkeysession):
 @pytest.fixture(scope="session")
 def secrets_manager_client():
     with mock_secretsmanager():
-        yield boto3.client("secretsmanager")
+        yield boto3.client("secretsmanager", region_name="us-east-1")
 
 
 @pytest.fixture
