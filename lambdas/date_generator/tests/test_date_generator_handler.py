784e9bf78b96e71d99b0292886c9051966659644
sharkinsspatial <sharkinsgis@gmail.com>
2021-08-09 13:59:13 -0500

    Reduce date search range now that IntHub backfill process is implemented.

--- lambdas/date_generator/tests/test_date_generator_handler.py
+++ lambdas/date_generator/tests/test_date_generator_handler.py
@@ -9,7 +9,7 @@ from handler import get_dates, handler
 @freeze_time("2020-01-22")
 def test_that_get_dates_returns_correct_dates():
     expected_dates = [
-        datetime(2020, 1, i).date().strftime("%Y-%m-%d") for i in range(21, 0, -1)
+        datetime(2020, 1, i).date().strftime("%Y-%m-%d") for i in range(21, 16, -1)
     ]
     actual_dates = get_dates()
     assert_that(expected_dates).is_equal_to(actual_dates)
@@ -18,7 +18,7 @@ def test_that_get_dates_returns_correct_dates():
 @freeze_time("2020-04-22")
 def test_that_date_generator_handler_returns_correct_dates():
     expected_dates = [
-        datetime(2020, 4, i).date().strftime("%Y-%m-%d") for i in range(21, 0, -1)
+        datetime(2020, 4, i).date().strftime("%Y-%m-%d") for i in range(21, 16, -1)
     ]
     expected_handler_output = {"query_dates": expected_dates}
     actual_handler_output = handler(None, None)
