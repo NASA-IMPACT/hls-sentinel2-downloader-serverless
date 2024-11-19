0316886dd3420b74342bf6b25c520660272d78ee
Chris Holden <ceholden@gmail.com>
2024-11-19 12:53:37 -0500

    Cleanup print statements

--- lambdas/link_fetcher/app/subscription_handler.py
+++ lambdas/link_fetcher/app/subscription_handler.py
@@ -13,5 +13,3 @@ logging.getLogger("app").setLevel(logging.INFO)
 config = EndpointConfig.load_from_secrets_manager(os.environ["STAGE"])
 app = build_app(config)
 handler = Mangum(app)
-
-print("BUILD THE LAMBDA HANDLER")
