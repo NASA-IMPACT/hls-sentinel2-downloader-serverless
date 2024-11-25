d5a8e5105156d293cdbb6484067d592227e2d3da
Chris Holden <ceholden@gmail.com>
2024-11-25 18:39:06 -0500

    Add 'platform' to GranuleCount table with (date, platform) pkey

--- alembic_migration/env.py
+++ alembic_migration/env.py
@@ -7,6 +7,8 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from sqlalchemy.engine import url
 
+from db.models.base import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -19,7 +21,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
