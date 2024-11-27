50a5116819eabbf56f463ed551ef5fbd59ad8372
Chris Holden <ceholden@gmail.com>
2024-11-26 19:18:21 -0500

    Revert "Mock scihub search supports platform query"
    This reverts commit 4841e3c5dfa183e4b1a7eafe9619db75d9525a2e.

--- lambdas/mock_scihub_search_api/handler.py
+++ lambdas/mock_scihub_search_api/handler.py
@@ -7,18 +7,15 @@ def handler(event, _):
     yesterday = datetime.now().date() - timedelta(days=1)
     yesterday_str = yesterday.strftime("%Y-%m-%d")
     params = event["queryStringParameters"]
-    platform = params["platform"]
 
     if yesterday_str not in params["publishedAfter"]:
-        response_fixture = f"scihub_response_platform_{platform}_no_results.json"
+        response_fixture = "scihub_response_no_results.json"
     elif (index := int(params["index"])) == 1:
-        response_fixture = f"scihub_response_platform_{platform}_index_1_yesterday.json"
+        response_fixture = "scihub_response_index_1_yesterday.json"
     elif index == 101:
-        response_fixture = (
-            f"scihub_response_platform_{platform}_index_101_yesterday.json"
-        )
+        response_fixture = "scihub_response_index_101_yesterday.json"
     else:
-        response_fixture = f"scihub_response_platform_{platform}_no_results.json"
+        response_fixture = "scihub_response_no_results.json"
 
     responses_dir = Path(__file__).with_name("scihub_responses")
     body = (responses_dir / response_fixture).read_text()
