6bc99b50502c4f3c04d963dfef4267f1ce1e195b
Chris Holden <ceholden@gmail.com>
2025-01-28 17:07:33 -0500

    Use event payload kwargs to support backfills AND unit tests

--- lambdas/date_generator/handler.py
+++ lambdas/date_generator/handler.py
@@ -1,38 +1,33 @@
 import os
-from collections.abc import Callable, Sequence
+from collections.abc import Sequence
 from datetime import datetime, timedelta
 from itertools import product
-from typing import TypedDict, Unpack
 
 
-class HandlerKwargs(TypedDict, total=False):
-    platforms: Sequence[str]
-    now: Callable[[], datetime]
-    lookback_days: int
-
-
+DATE_FORMAT_YMD = "%Y-%m-%d"
 DEFAULT_LOOKBACK_DAYS = 5
 
 
 def handler(
-    _event,
+    event: dict,
     _context,
-    **kwargs: Unpack[HandlerKwargs],
 ):
     """
     Return a `dict` with the single key `'query_dates_platforms'` mapped to a list of
     2-tuples of the form `(date, platform)` produced from the cross-product of dates
-    given by `get_dates` (for `kwargs['lookback_days']` number of days _prior_ to the
-    date given by `kwargs['now']()`) and `kwargs['platforms']`.
+    given by `get_dates` (for `event['lookback_days']` number of days _prior_ to the
+    date given by `event['now']`) and `event['platforms']`.
 
-    NOTE: AWS will never pass kwargs.  They are for unit testing purposes only, but
-    since they are all optional, AWS can safely call this function with none of them.
+    NOTE: Our StepFunction will never pass these kwargs in the payload by default.
+    They are for backfill and unit testing purposes only, but since they are all
+    optional, our daily scheduled StepFunction can safely call this function with none
+    of them.
 
     Raises
     ------
-    KeyError: if the `platforms` kwarg is not specified and the environment variable
-        `PLATFORMS` is not defined
-    ValueError: if the `platforms` kwarg is an empty sequence, or it is not specified
+    KeyError: if `platforms` is not specified in the handler payload and the environment
+        variable `PLATFORMS` is not defined
+    ValueError: if the `platforms` input is an empty sequence, or it is not specified
         and the `PLATFORMS` environment variable is set to a value that is either
         empty, only whitespace, or a combination of commas and whitespace
 
@@ -42,36 +37,30 @@ def handler(
     number of platforms, regardless of the current date:
 
     >>> platforms = ("S2A", "S2B", "S2C")
-    >>> combos = handler(None, None, platforms=platforms)["query_dates_platforms"]
+    >>> combos = handler({"platforms": platforms}, None)["query_dates_platforms"]
     >>> len(combos) == DEFAULT_LOOKBACK_DAYS * len(platforms)
     True
 
     For a known date and number of lookback days, we can enumerate the exact combos:
 
     >>> handler(  # doctest: +NORMALIZE_WHITESPACE
+    ...     {"platforms": platforms, "now": "2024-03-02", "lookback_days": 3},
     ...     None,
-    ...     None,
-    ...     platforms=platforms,
-    ...     now=lambda: datetime(2024, 3, 2),
-    ...     lookback_days=3
     ... )
     {'query_dates_platforms':
      [('2024-03-01', 'S2A'), ('2024-03-01', 'S2B'), ('2024-03-01', 'S2C'),
       ('2024-02-29', 'S2A'), ('2024-02-29', 'S2B'), ('2024-02-29', 'S2C'),
       ('2024-02-28', 'S2A'), ('2024-02-28', 'S2B'), ('2024-02-28', 'S2C')]}
     """
-
-    default_kwargs: HandlerKwargs = {
-        "now": datetime.now,
-        "lookback_days": DEFAULT_LOOKBACK_DAYS,
-    }
-    kwargs = default_kwargs | kwargs
-
     # We want to fail if neither platforms is supplied as a kwarg (during testing) nor
     # PLATFORMS is defined as a (non-empty) environment variable.
-    platforms = kwargs.get("platforms") or parse_platforms(os.environ["PLATFORMS"])
-    now = kwargs["now"]
-    lookback_days = kwargs["lookback_days"]
+    platforms = event.get("platforms") or parse_platforms(os.environ["PLATFORMS"])
+    # By default "now" should be today to support cron usage, but allow overrides
+    # for backfill jobs
+    now = datetime.strptime(
+        event.get("now", datetime.now().strftime(DATE_FORMAT_YMD)), DATE_FORMAT_YMD
+    )
+    lookback_days = event.get("lookback_days", DEFAULT_LOOKBACK_DAYS)
 
     return {
         "query_dates_platforms": list(product(get_dates(now, lookback_days), platforms))
@@ -127,25 +116,25 @@ def parse_platforms(platforms: str) -> Sequence[str]:
     return result
 
 
-def get_dates(now: Callable[[], datetime], lookback_days: int) -> Sequence[str]:
+def get_dates(now: datetime, lookback_days: int) -> Sequence[str]:
     """
     Return one date string per day for `lookback_days` number of days, in reverse
-    chronological order, starting from the day before `now()` and formatted as
+    chronological order, starting from the day before `now` and formatted as
     `%Y-%m-%d`.
 
     Examples
     --------
-    >>> len(get_dates(datetime.now, 10)) == 10
+    >>> len(get_dates(datetime.now(), 10)) == 10
     True
-    >>> get_dates(lambda: datetime(2025, 1, 3), 3)
+    >>> get_dates(datetime(2025, 1, 3), 3)
     ['2025-01-02', '2025-01-01', '2024-12-31']
 
     :returns: string dates (`%Y-%m-%d`) looking back the number of days given by
         `lookback_days` in reverse chronological order starting from the day before
         `now()`
     """
-    yesterdays_date = now().date() - timedelta(days=1)
+    yesterdays_date = now.date() - timedelta(days=1)
     return [
-        (yesterdays_date - timedelta(days=day)).strftime("%Y-%m-%d")
+        (yesterdays_date - timedelta(days=day)).strftime(DATE_FORMAT_YMD)
         for day in range(lookback_days)
     ]
